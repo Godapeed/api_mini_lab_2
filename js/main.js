da6527562f95118b49bd98330f8b964106cfb4fc
Kirill <morozoff.gamemode@yandex.ru>
2023-11-20 18:52:33 +0300

    Проверка валидности, Блокировка кнопки, Форма авторизации

--- js/main.js
+++ js/main.js
@@ -1,18 +1,16 @@
-import {setFormValue, submitSignUpForm, validateEmail, validatePassword} from "./utils.js"
+import {setFormValue, submitSignUpForm, validateEmail, validatePassword, validateRepeatPassword, submitSignInForm, checkFirstName} from "./utils.js"
 
 
 ////// ДЕМОНСТРАЦИОННЫЙ УЧАСТОК КОДА. На оценку не влияет, исключительно для саморазвития.
 
 // Предлагаю "поиграться" с частями кода ниже, чтобы познакомиться с JS
 // Получаем элемент и меняем его класс, который определеён в библиотеке стилей materialize
-const password = document.getElementById('password');
-password.classList.add("valid")
-password.classList.remove("valid")
+//const password = document.getElementById('password');
+//password.classList.add("valid")
+//password.classList.remove("valid")
 
 // В браузере можно посмотреть, что из себя представляет документ
 // (CTRL+SHIFT+i для открытия консоли и открыть вкладку "консоль", туда будет залогированно значение)
-console.log("Document")
-console.log(document)
 
 // Если запросить id, которого нет в DOM дереве - вернется undefined
 // => надо быть осторожней: коллега может поменять id вашего элемента и упадёт !ВАШ! код
@@ -42,6 +40,12 @@ const sign_up_form_id = 'sign_up_form'
 const sign_up_btn_id = 'sign_up_btn'
 const sign_in_form_id = 'sign_in_form'
 
+const password_repeat_id = "password-repeat"
+
+const first_name_si_id = "first_name_si"
+const last_name_si_id = "last_name_si"
+const password_si_id = "password_si"
+const sign_up_btn_si_id = "sign_up_btn_si"
 
 // Получаем элемент DOM-дерева по id и присваиваем значение аттрибуту oninput
 // oninput вызывается с параметром "event" каждый раз, когда ввод меняется
@@ -51,10 +55,32 @@ const sign_in_form_id = 'sign_in_form'
 const first_name = document.getElementById(first_name_id);
 first_name.oninput = (e) => setFormValue(first_name_id, e.target.value)  // Установить значение без валидации
 
+const last_name = document.getElementById(last_name_id);
+last_name.oninput = (e) => setFormValue(last_name_id, e.target.value)
+
 const email = document.getElementById(email_id);
 email.oninput = (e) => setFormValue(email_id, e.target.value, validateEmail) // Установить значение с валидацией
 
+const password = document.getElementById(password_id);
+password.oninput = (e) => setFormValue(password_id, e.target.value, validatePassword)
 
+const repassword = document.getElementById(password_repeat_id);
+repassword.oninput = (e) => setFormValue(password_repeat_id, e.target.value, validateRepeatPassword)
+
+const sign_up_btn = document.getElementById(sign_up_btn_id);
+sign_up_btn.disabled = true;
+
+const first_name_si = document.getElementById(first_name_si_id);
+first_name_si.oninput = (e) => setFormValue(first_name_si_id, e.target.value, checkFirstName)
+
+const last_name_si = document.getElementById(last_name_si_id);
+last_name_si.oninput = (e) => setFormValue(last_name_si_id, e.target.value, checkFirstName)
+
+const password_si = document.getElementById(password_si_id);
+password_si.oninput = (e) => setFormValue(password_si_id, e.target.value, checkFirstName)
+
+const sign_up_btn_si = document.getElementById(sign_up_btn_si_id);
+sign_up_btn_si.disabled = true;
 
 // Меняем стили объекта DOM дерева. Это позволяет скрыть форму регистрации и показать форму авторизации
 // Объект формы не исключается из DOM дерева, а просто становистя невидимым
@@ -65,7 +91,6 @@ switch_to_sign_in.onclick = (e) => {
 }
 
 
-const sign_up_btn = document.getElementById(sign_up_btn_id);
 sign_up_btn.onclick = (e) => {
   // При нажатии кнопки в форме по умолчанию происходит перезагрузка страницы.
   // Чтобы отключить его, нужно отменить стандартное поведение события
@@ -73,3 +98,7 @@ sign_up_btn.onclick = (e) => {
   submitSignUpForm()
 }
 
+sign_up_btn_si.onclick = (e) => {
+  e.preventDefault()
+  submitSignInForm()
+}
--- js/utils.js
+++ js/utils.js
@@ -1,57 +1,160 @@
-const formValues = {}  // Сюда пишутся значения формы (Object как в Java, или dict из Python)
-const formValidation = {}  // Сюда пишутся статусы валидации каждого поля. Если поле ни разу не валидировалось,
+const formValues = {}; // Сюда пишутся значения формы (Object как в Java, или dict из Python)
+const formValidation = {}; // Сюда пишутся статусы валидации каждого поля. Если поле ни разу не валидировалось,
 // то при обращении к Object вернётся undefined, который при логическом сравнении обрабатывается как false
-
 
 // Объявляется и инициализируется константная переменная
 // Инициализация функцией, заданной в стрелочном виде
 export const validatePassword = (e) => {
-  formValidation.password = e.target.value
-  console.log("Password validation...")
-  console.log(e)
+  //Пароль должен содержать 8 символов и хотя бы одну цифру, одну букву и один уникальный символ
+  const regExp = /^.*(?=.{8,})(?=.*[a-zA-Z])(?=.*\d)(?=.*[!#$%&? "]).*$/;
   // Напишите код валидации здесь и присвойте true/false в объект(словарь) formValidation
   // formValidation.password = ...  // formValidation['password'] = ... - то же самое, но другой синтаксис
-  return formValidation.password !== undefined   // Это заглушка, return вероятно надо переписать
-}
+  const repassword = document.getElementById("password-repeat");
+  repassword.classList.add("invalid");
+  checkValidationStatus()
+  if (String(e).match(regExp) !== null) {
+    password.classList.remove("invalid");
+    password.classList.add("valid");
+    return true;
+  } else {
+    password.classList.remove("valid");
+    password.classList.add("invalid");
+    return false;
+  }
+};
 
+export const checkValidationStatus = () => {
+  const repassword = document.getElementById("password-repeat");
+  if (
+    !getValidationStatus() &&
+    formValidation[email] !== null &&
+    formValidation[password] !== null &&
+    formValidation[repassword] !== null
+  ) {
+    sign_up_btn.disabled = false;
+  } else {
+    sign_up_btn.disabled = true;
+  }
+};
 
-export const validateEmail = (email) => {
+export const validateRepeatPassword = (e) => {
+  const repassword = document.getElementById("password-repeat");
+  checkValidationStatus()
+  if (e === password.value) {
+    repassword.classList.remove("invalid");
+    repassword.classList.add("valid");
+    return true;
+  } else {
+    repassword.classList.remove("valid");
+    repassword.classList.add("invalid");
+    return false;
+  }
+};
+
+export const validateEmail = (e) => {
   // Создадим шаблон регулярного выражения. В нём применяются шаблонные строки
   // Гуглить по тегам: "шаблонные строки js", "регулярные выражения"
-  const regExp = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+  const regExp =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-  return String(email)
-    .toLowerCase()
-    .match(regExp);
-}
+    checkValidationStatus()
 
+  return String(e).toLowerCase().match(regExp);
+};
 
 // Функция возвращающая true если все валидации пройдены, и false если хотя бы одна не пройдена
 export const getValidationStatus = () => {
   // Происходит функциональная мгаия, читай строчку кода ниже как:
   // Получить значения (не ключи) из объекта, затем применить к каждому значению функцию двойного логического отрицания
   // (преобразование к булевому типу) и результаты всех применений это true, то вернуть true, иначе - false
-  return Object.values(formValidation).every((validationStatus) => !!validationStatus)
-}
-
+  return Object.values(formValidation).every(
+    (validationStatus) => !!validationStatus
+  );
+};
 
 // Функция возвращающая которая ставит значение поля в форме по ключу
 export const setFormValue = (valueKey, newValue, validator) => {
-  formValues[valueKey] = newValue
+  formValues[valueKey] = newValue;
   if (validator !== undefined) {
-    formValidation[valueKey] = validator(newValue)
+    formValidation[valueKey] = validator(newValue);
   }
-}
-
+};
 
 // Функция для обработки отправки формы регистрации
 // В этой функции должен быть http запрос на сервер для регистрации пользователя (сейчас просто демонстрация)
 export const submitSignUpForm = () => {
   if (!getValidationStatus()) {
-    console.log("FORM IS INCORRECT")
-    return false
+    console.log("FORM IS INCORRECT");
+    return false;
   }
-  console.log("FORM IS FINE")
-  console.log(formValues)
-  return true
+  console.log("FORM IS FINE");
+  console.log(formValues);
+  return true;
+};
+
+const SingInUsers = [
+  { first_name: "admin", last_name: "admin", password: "1234" },
+  { first_name: "admin", last_name: "admin", password: "admin" },
+];
+
+export const checkFirstName = (e) => {
+  const first_name_si_id = document.getElementById("first_name_si");
+  const last_name_si_id = document.getElementById("last_name_si");
+  const password_si_id = document.getElementById("password_si");
+  if (first_name_si_id.value !== '' && last_name_si_id.value !== '' && password_si_id.value !== '') {
+    sign_up_btn_si.disabled = false;
+  } else {
+    sign_up_btn_si.disabled = true;
+  }
 }
+
+export const submitSignInForm = () => {
+  const first_name_si_id = document.getElementById("first_name_si");
+  const last_name_si_id = document.getElementById("last_name_si");
+  const password_si_id = document.getElementById("password_si");
+
+  var find_first_name = false;
+  var find_last_name = false;
+  var find_password = false;
+  for (let i in SingInUsers) {
+    for (let j in SingInUsers[i]) {
+      if (first_name_si_id.value === SingInUsers[i][j] && j === "first_name") {
+        find_first_name = true;
+      }
+      if (last_name_si_id.value === SingInUsers[i][j] && j === "last_name") {
+        find_last_name = true;
+      }
+      if (password_si_id.value === SingInUsers[i][j] && j === "password") {
+        find_password = true;
+      }
+    }
+  }
+  if (!find_first_name) {
+    first_name_si_id.classList.remove("valid");
+    first_name_si_id.classList.add("invalid");
+  } else {
+    first_name_si_id.classList.remove("invalid");
+    first_name_si_id.classList.add("valid");
+  }
+  if (!find_last_name) {
+    last_name_si_id.classList.remove("valid");
+    last_name_si_id.classList.add("invalid");
+  } else {
+    last_name_si_id.classList.remove("invalid");
+    last_name_si_id.classList.add("valid");
+  }
+  if (!find_password) {
+    password_si_id.classList.remove("valid");
+    password_si_id.classList.add("invalid");
+  } else {
+    password_si_id.classList.remove("invalid");
+    password_si_id.classList.add("valid");
+  }
+  if (find_first_name && find_last_name && find_password) {
+    console.log("Sing in correct");
+    return true;
+  } else {
+    console.log("Sing in not correct");
+    return false;
+  }
+};
